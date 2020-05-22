a5bb9d44b59e0d0eed381fbff554d8e3b0384c55
dulani <dmcmaheshi@gmail.com>
2020-05-22 21:02:07 +0530

    Update Counter.js

--- src/Component/Counter.js
+++ src/Component/Counter.js
@@ -9,22 +9,36 @@ export class Counter extends Component {
         }
     }
     
+// increment(){
+//     this.setState({
+//         count: this.state.count + 1
+//     },
+//     () => {
+//         console.log('This is callback', this.state.count)
+//     }
+//     )
+//     console.log(this.state.count)
+// }
+
 increment(){
-    this.setState({
-        count: this.state.count + 1
-    },
-    () => {
-        console.log('This is callback', this.state.count)
-    }
-    )
+    this.setState((prevState) => ({
+        count : prevState.count + 1
+    }))
     console.log(this.state.count)
+}
+
+incrementfive(){
+    this.increment()
+    this.increment()
+    this.increment()
+    this.increment()
 }
 
     render() {
         return (
             <div>
                <h3>Count - {this.state.count}</h3>
-               <button onClick = {() => this.increment()}>Increment</button>
+               <button onClick = {() => this.incrementfive()}>Increment</button>
             </div>
         )
     }
